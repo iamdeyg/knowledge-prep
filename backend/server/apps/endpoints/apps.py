b57ca0547cc509d881d81f59ace87a896ea70364
iamdeyg <deygderin@gmail.com>
2021-11-10 14:21:35 +0100

    add ml code

--- backend/server/apps/endpoints/apps.py
+++ backend/server/apps/endpoints/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class EndpointsConfig(AppConfig):
-    name = 'endpoints'
+    'django.db.models.AutoField'
+    name = 'apps.endpoints'
--- backend/server/apps/endpoints/views.py
+++ backend/server/apps/endpoints/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
-from apps.endpoints.models import Endpoint
+from .models import Endpoint
 from apps.endpoints.serializers import EndpointSerializer
 
 from rest_framework.exceptions import APIException
